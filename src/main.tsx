eca6a3866af98f72889e70e8bf4f703eeb118e33
1krasyuk <evgeniyk687@gmail.com>
2026-07-30 02:20:14 +0300

    chore: configure file-based routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
 
 import "./index.css"
-import App from "./app/App.tsx"
+
 import { ThemeProvider } from "@/app/providers/theme-provider.tsx"
+import { routeTree } from "./routeTree.gen"
+
+const router = createRouter({ routeTree })
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 )
