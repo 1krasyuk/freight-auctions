4c5155adee2276d6ad8042d6819c4d244d837796
1krasyuk <evgeniyk687@gmail.com>
2026-08-02 11:27:42 +0300

    feat(mocks): implement auction bet mutation

--- src/entities/auction/api/mocks/handlers/index.ts
+++ src/entities/auction/api/mocks/handlers/index.ts
@@ -1,9 +1,11 @@
 import { getAuctionHandler } from "./get-auction-handler"
 import { listAuctionBetsHandler } from "./list-auction-bets-handler"
 import { listAuctionsHandler } from "./list-auctions-handler"
+import { setAuctionBetHandler } from "./set-auction-bet-handler"
 
 export const auctionHandlers = [
   listAuctionsHandler,
   getAuctionHandler,
   listAuctionBetsHandler,
+  setAuctionBetHandler,
 ]
